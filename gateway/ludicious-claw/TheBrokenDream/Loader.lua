9092eb551fde910ffc64a8aa80d3836c6286588a
ludicious-claw <ludicious_claw@tutamail.com>
2026-05-25 17:33:22 +0500

    Update Loader.lua

--- gateway/ludicious-claw/TheBrokenDream/Loader.lua
+++ gateway/ludicious-claw/TheBrokenDream/Loader.lua
@@ -1,1 +1,15 @@
-
+if game.PlaceId == 96775630583143 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Unwalker1337/nullfire/refs/heads/main/gateway/unwalker/TheBrokenDream/Chapter_1/ItsJustGettingStarted.lua"))()
+elseif game.PlaceId == 108798177610650 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Unwalker1337/nullfire/refs/heads/main/gateway/unwalker/TheBrokenDream/Chapter_1/CH0.lua"))()
+elseif game.PlaceId == 74323372320017 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Unwalker1337/nullfire/refs/heads/main/gateway/unwalker/TheBrokenDream/Chapter_1/CLOUDS.lua"))()
+elseif game.PlaceId == 77782724453274 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Unwalker1337/nullfire/refs/heads/main/gateway/unwalker/TheBrokenDream/Chapter_1/educational_purposes.lua"))()
+elseif game.PlaceId == 110334679443599 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Unwalker1337/nullfire/refs/heads/main/gateway/unwalker/TheBrokenDream/Chapter_1/dread.lua"))()
+elseif game.PlaceId == 129251655434632 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Unwalker1337/nullfire/refs/heads/main/gateway/unwalker/TheBrokenDream/Chapter_1/glass_breach.lua"))()
+else
+    game.Players.LocalPlayer:Kick("wrong game...")
+end
